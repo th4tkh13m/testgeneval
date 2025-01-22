564436e40dc809ce8ce5052201aa49c0e26072c1
khangtran2020 <kt36@njit.edu>
2025-01-22 13:15:01 -0500

    fix keys

--- swebench_docker/evaluate_instance.py
+++ swebench_docker/evaluate_instance.py
@@ -11,7 +11,7 @@ import sys
 import importlib
 
 # install coverage.py
-subprocess.check_call([sys.executable, "-m", "pip", "install", "coverage"])
+subprocess.check_call(["python", "-m", "pip", "install", "coverage"])
 
 from typing import Optional, Tuple
 from coverage import CoverageData
