c2fa95e06940b7887642e4e18029989dfa3c587b
khangtran2020 <kt36@njit.edu>
2025-01-30 15:44:20 -0500

    add debugger

--- run_eval_testcase.py
+++ run_eval_testcase.py
@@ -132,9 +132,7 @@ async def main(
             branch_key = "branch_translate"
             test_case_key = "translate"
         logger.info(f"================== Task {res[KEY_ID]} ==================")
-        logger.info(
-            f"Task instance branches at setting {setting}: {res[branch_key][setting]}"
-        )
+        logger.info(f"Task instance branches at setting {setting}: {res[branch_key]}")
         logger.info(
             f"Task {res[KEY_ID]} orignally has {len(task_dict[res[KEY_ID]][branch_key])} branches and {len(task_dict[res[KEY_ID]][test_case_key])} test cases"
         )
